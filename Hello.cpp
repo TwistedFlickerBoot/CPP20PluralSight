f3dd244837d08e3579d75ad7306939f55a5e8e5e
MerlinTheSpook <chrissynrick@yahoo.com>
2025-06-03 12:10:15 -0700

    Add Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Type your name: " << "\n";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << "\n";
+
+    return 0;
+}
