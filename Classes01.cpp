0f017a770f2daf3d0e93e5bc8d957af0aac23a31
MerlinTheSpook <chrissynrick@yahoo.com>
2025-06-04 10:27:16 -0700

    Update Classes01.cpp

--- Classes01.cpp
+++ Classes01.cpp
@@ -8,6 +8,8 @@ private:
     std::string lastName;
     int arbitraryNumber;
 public:
+    Person(std::string first, std::string last, int arbitrary)
+        : firstName(first), lastName(last), arbitraryNumber(arbitrary) {}
     std::string getName();
     void setName(const std::string& first, const std::string& last) {
         firstName = first;
@@ -22,7 +24,7 @@ std::string Person::getName()
 
 int main()
 {
-    Person person;
+    Person person("Jane", "Doe", 42);
     person.setName("John", "Doe");
     std::cout << "Person's name: " << person.getName() << std::endl;
     return 0;
